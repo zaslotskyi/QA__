ff4ddf052ac8ab0bd4ff38060bc7f6606d985892
ruslan.zaslotskyi <r.zaslotskiy@gmail.com>
2024-04-18 20:03:43 +0300

    lesson2.py added

---
+++ lesson3.py
@@ -0,0 +1,11 @@
+my_list = ["Ukraine", "Spain", "Poland"]
+
+
+my_dict = {
+    "Ukraine": "Kyiv",
+    "Spain": "Madrid",
+    "Poland": "Warsaw"
+}
+print(my_list[0], my_dict["Ukraine"], sep= ": ")
+print(my_list[1], my_dict["Spain"], sep= ": ")
+print(my_list[2], my_dict["Poland"], sep= ": ")
