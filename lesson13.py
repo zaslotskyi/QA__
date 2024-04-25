8385815a79e965eb2bfe48f903458118b1ddfe84
ruslan.zaslotskyi <r.zaslotskiy@gmail.com>
2024-04-25 17:07:28 +0300

    add func can_be_value for HW13

--- lesson13.py
+++ lesson13.py
@@ -1,24 +1,34 @@
 import string
 
-email = "aaa@bbb.ccc"
 
-found = False
-
-if (email.index("@") < email.index(".")
-        and email[0] != "@"
-        and email[-1] != "."
-        and email.count("@") == 1
-        and email.count(".") == 1):
-    for char in email:
-        if char not in string.ascii_letters + string.digits + "@.":
-            found = True
-            break
-    if not found:
-        print(True)
+def can_be_email(email):
+    found = False
+    if (email.index("@") < email.index(".")
+            and email[0] != "@"
+            and email[-1] != "."
+            and email.count("@") == 1
+            and email.count(".") == 1):
+        for char in email:
+            if char not in string.ascii_letters + string.digits + "@.":
+                found = True
+                break
+        if not found:
+            return True
+        else:
+            return False
     else:
-        print(False)
-else:
-    print(False)
+        return False
+
+
+
+assert can_be_email("aaa@bbb.ccc") == True, 'Test1'
+assert can_be_email("@aaa@bbb.ccc") == False, 'Test2'
+assert can_be_email("aaa@bbb.ccc.") == False, 'Test3'
+assert can_be_email("aaa777@bbb.ccc") == True, 'Test4'
+assert can_be_email("aaa.bbb@ccc") == False, 'Test5'
+print('Passed')
+
+
 
 
 
