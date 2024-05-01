e91a77c2542851a422bb19c7f799676a617ced14
ruslan.zaslotskyi <r.zaslotskiy@gmail.com>
2024-05-01 16:52:27 +0300

    add HW14

---
+++ lesson14.py
@@ -0,0 +1,24 @@
+my_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+
+
+divisible_by_3 = []
+divisible_by_5 = []
+divisible_by_3_and_5 = []
+
+
+for i in my_list:
+    if i % 3 == 0 and i % 5 != 0:
+        divisible_by_3.append(i)
+    elif i % 5 == 0 and i % 3 != 0:
+        divisible_by_5.append(i)
+    elif i % 3 == 0 and i % 5 == 0:
+        divisible_by_3_and_5.append(i)
+
+
+
+assert divisible_by_3 == [3, 6, 9, 12], 'Test1'
+assert divisible_by_5 == [5, 10], 'Test2'
+assert divisible_by_3_and_5 == [0, 15], 'Test3'
+print("Passed")
+
+
