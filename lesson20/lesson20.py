a6b2a3a53534d76c4ed24a37ada1a9587b1155f1
ruslan.zaslotskyi <r.zaslotskiy@gmail.com>
2024-05-09 21:56:10 +0300

    fix HW20

--- lesson20/lesson20.py
+++ lesson20/lesson20.py
@@ -1,12 +1,11 @@
 def longest_line_in_file(file):
-    with open(f"{file}", "r") as file:
+    with open(file, "r") as file:
         max_length = 0
         longest_line = ""
         for line in file:
-            if len(line) > max_length:
-                max_length = len(line)
-                longest_line = line
+            if len(line.strip()) >= max_length:
+                max_length = len(line.strip())
+                longest_line = line.strip()
     return longest_line
 
 print(longest_line_in_file('lesson20file.txt'))
-
