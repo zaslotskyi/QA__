5e21e894bcc5b7901739c87d0ad295d2f1ad74d9
ruslan.zaslotskyi <r.zaslotskiy@gmail.com>
2024-05-13 17:53:44 +0300

    fix HW21

--- lesson21/lesson21.py
+++ lesson21/lesson21.py
@@ -1,21 +1,20 @@
-def smallest_files(dict_data):
+import os
+
+
+def smallest_files(directory_path):
 
     with open('combined_files.txt', 'w') as f:
-        for filename, content in dict_data.items():
+        directory_path = os.path.abspath(directory_path)
+        for file_name in os.listdir(directory_path):
+            file_path = os.path.join(directory_path, file_name)
+            with open(file_path, 'r') as txt_file:
+                content = txt_file.read()
             if len(content.encode('utf-8')) <= 120:
-                f.write(f"Filename: {filename}\n")
+                f.write(f"Filename: {file_name}\n")
                 f.write(content + "\n")
                 f.write("-" * 50 + "\n")
 
 
+smallest_files('files')
 
 
-smallest_files({
-    'file1.txt': 'You have source_directory with nested directories (any level of nesting). '
-                 'In all those directories could be text files (with extension *.txt) and any other files.'
-                 'Create file combined_files.txt.',
-    'file2.txt': 'You have source_directory with nested directories (any level of nesting).'
-                 'testtesttest',
-    'file3.txt': 'You have source_directory with nested directories (any level of nesting).'
-
-})
