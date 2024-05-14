0542bf071d6344ece1e8025c543b7134beb4c591
ruslan.zaslotskyi <r.zaslotskiy@gmail.com>
2024-05-14 16:19:07 +0300

    fix HW21

--- lesson21/lesson21.py
+++ lesson21/lesson21.py
@@ -1,20 +1,16 @@
 import os
 
-
 def smallest_files(directory_path):
 
     with open('combined_files.txt', 'w') as f:
-        directory_path = os.path.abspath(directory_path)
-        for file_name in os.listdir(directory_path):
-            file_path = os.path.join(directory_path, file_name)
-            with open(file_path, 'r') as txt_file:
-                content = txt_file.read()
-            if len(content.encode('utf-8')) <= 120:
-                f.write(f"Filename: {file_name}\n")
-                f.write(content + "\n")
-                f.write("-" * 50 + "\n")
-
+        for root, dirs, files in os.walk(directory_path):
+            for file_name in files:
+                file_path = os.path.join(root, file_name)
+                with open(file_path, 'r') as txt_file:
+                    content = txt_file.read()
+                if len(content.encode('utf-8')) <= 120:
+                    f.write(f"Filename: {file_name}\n")
+                    f.write(content + "\n")
+                    f.write("-" * 50 + "\n")
 
 smallest_files('files')
-
-
